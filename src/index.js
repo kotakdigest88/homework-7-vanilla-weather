4f2a72bd7d49bf9bb1e3731eb156c6c1e186a9d3
nsalina <nsalina76@gmail.com>
2024-01-12 15:26:45 +0800

    added axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-function handleSearchSubmit(event){
+function showWeather (response){
+    let temperatureElement = document.querySelector("#temperature-value");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+   
+}
+
+
+function searchCity (city){
+    let apiKey ="2c9e95a4078b004c2cb60t37off36a27";
+    let urlKey =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(urlKey).then(showWeather);
+}
+
+
+
+
+function handleSearchSubmit (event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-app-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
