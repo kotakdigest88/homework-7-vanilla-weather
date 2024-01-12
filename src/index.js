3f78bfe85411a75491d6e2401e896a0e8fdd3857
nsalina <nsalina76@gmail.com>
2024-01-13 04:55:02 +0800

    added more details on weather

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 function showWeather (response){
+console.log(response.data);
 
-
-    let temperatureElement = document.querySelector("#temperature-value");
+    let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature);
     let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = response.data.city;
-   
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time *1000);
+    let icon =`<img src ="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;  
+    timeElement.innerHTML = formatDate (date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    
 }
+
+
+function formatDate (date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+        
+        if (minutes<10){
+            minutes = `0${minutes}`
+        }
+        if (hours<10){
+            hours = `0${hours}`;
+        }
+        return `${day} ${hours}:${minutes}`;
+        
+    }
 
 
 function searchCity (city){
@@ -15,9 +46,6 @@ function searchCity (city){
     let urlKey =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(urlKey).then(showWeather);
 }
-
-
-
 
 function handleSearchSubmit (event){
     event.preventDefault();
